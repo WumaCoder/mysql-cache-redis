ec4f328259c55ce9d31afee45d9bbfaf29e29711
刘欢 <15804854160@163.com>
2019-04-16 17:48:44 +0800

    0.0.1版本发布

--- src/api/test.GET.js
+++
@@ -1,6 +0,0 @@
-module.exports =async (req,res)=>{
-    let mc = mysqlCache;
-    let data = await mc.sqlQuery('select  *  from  goodproteins where ko=?;',['K0119'],true);
-    console.log(data);
-    res.send(data).end();
-}
--- src/config/mysql.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    host:'localhost',
-    post:3306,
-    user:'root',
-    password:'12345678',
-    database:'khjldb',
-    connectionLimit:10
-}
